ca21f4f77eede85b58fa254449d8a701d709fbb3
Thanh An <Nguyenthanhan26.qngai@gmail.com>
2024-10-18 14:00:43 +0700

    update

--- winproject/win-project-2/Models/User.cs
+++ winproject/win-project-2/Models/User.cs
@@ -14,13 +14,26 @@ namespace win_project_2.DAO
         public string Password { get; set; }
         public string Role { get; set; } 
 
-        public User(int userId, string name, string email, string password, string role)
+        public string Address {  get; set; }
+        public DateTime birthDate { get; set; }
+        public string PhoneNumber {  get; set; }
+        public string image {  get; set; }
+        public DateTime createDate {  get; set; }
+        public DateTime updateDate { get; set; }
+
+        public User(int userID, string name, string email, string password, string role, string address, DateTime birthDate, string phoneNumber, string image, DateTime createDate, DateTime updateDate)
         {
-            UserID = userId;
+            UserID = userID;
             Name = name;
             Email = email;
             Password = password;
             Role = role;
+            Address = address;
+            this.birthDate = birthDate;
+            PhoneNumber = phoneNumber;
+            this.image = image;
+            this.createDate = createDate;
+            this.updateDate = updateDate;
         }
 
         public override string ToString()
