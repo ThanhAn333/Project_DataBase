63827b20be5d3cfd5349e880467d32b06add6102
HieuDuong <duonghieu.7104@gmail.com>
2024-10-30 11:16:52 +0700

    xem các job đã đăng

--- winproject/win-project-2/Forms/Recruiter/UC/UCHomeRe.cs
+++ winproject/win-project-2/Forms/Recruiter/UC/UCHomeRe.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using win_project_2.DAO;
+using win_project_2.Models;
+using win_project_2.Service;
 
 namespace win_project_2.Forms.Recruiter.UC
 {
@@ -21,7 +23,23 @@ namespace win_project_2.Forms.Recruiter.UC
 
         void LoadAllJob()
         {
-            
+            JobDAO jobDAO = new JobDAO();
+            List<Job> jobs = jobDAO.GetJobsByEmployer(UserDangNhap.userId);
+
+            fpanelJob.Controls.Clear();
+
+            foreach (Job job in jobs)
+            {
+                UC_Job uc_Job = new UC_Job();
+                uc_Job.thongtin(
+                    job.Title,
+                    job.Salary,
+                    job.Location,
+                    job.Company
+                );
+
+                fpanelJob.Controls.Add(uc_Job);
+            }
         }
     }
 }
