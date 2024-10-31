f269d3a6e03afe382814b90c3f99b299a01f09b2
HieuDuong <duonghieu.7104@gmail.com>
2024-10-31 11:47:26 +0700

    f rating

--- winproject/win-project-2/Models/Review.cs
+++ winproject/win-project-2/Models/Review.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Firebase.Auth;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,27 +10,19 @@ namespace win_project_2.Models
 {
     public class Review
     {
-        public int ReviewID { get; set; }
-        public User Reviewer { get; set; }
-        public Job Job { get; set; }
+        public int UserID { get; set; }
+        public int JobID { get; set; }
         public int Rating { get; set; }
         public string Comment { get; set; }
         public DateTime ReviewDate { get; set; }
 
-        public Review(int reviewId, User reviewer, Job job, int rating, string comment, DateTime reviewDate)
+        public Review(int userID, int jobID, int rating, string comment, DateTime reviewDate)
         {
-            ReviewID = reviewId;
-            Reviewer = reviewer;
-            Job = job;
+            UserID = userID;
+            JobID = jobID;
             Rating = rating;
             Comment = comment;
             ReviewDate = reviewDate;
         }
-
-        public override string ToString()
-        {
-            return $"{Reviewer.Name} rated job {Job.Title} with {Rating} stars";
-        }
     }
-
 }
