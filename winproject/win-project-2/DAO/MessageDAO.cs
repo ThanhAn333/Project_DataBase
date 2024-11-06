e0626162a69cd8d8e6b88526d8b51c8710b77157
HieuDuong <duonghieu.7104@gmail.com>
2024-11-06 08:45:45 +0700

    load conversation

--- winproject/win-project-2/DAO/MessageDAO.cs
+++ winproject/win-project-2/DAO/MessageDAO.cs
@@ -45,5 +45,40 @@ namespace win_project_2.DAO
                 }
             }
         }
+
+        public List<Message> LoadMessages(int user1ID, int user2ID)
+        {
+            List<Message> messages = new List<Message>();
+
+            using (SqlConnection conn = dbConn.GetConnection())
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("GetMessagesBetweenUsers", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@User1ID", user1ID);
+                    cmd.Parameters.AddWithValue("@User2ID", user2ID);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int messageId = reader.GetInt32(reader.GetOrdinal("MessageID"));
+                            int senderId = reader.GetInt32(reader.GetOrdinal("SenderID"));
+                            int receiverId = reader.GetInt32(reader.GetOrdinal("ReceiverID"));
+                            string content = reader.GetString(reader.GetOrdinal("Content"));
+                            DateTime timestamp = reader.GetDateTime(reader.GetOrdinal("Timestamp"));
+
+                            Message message = new Message(messageId, senderId, receiverId, content, timestamp);
+                            messages.Add(message);
+                        }
+                    }
+                }
+            }
+
+            return messages;
+        }
+
     }
 }
--- winproject/win-project-2/Forms/UCComponents/FormChat.cs
+++ winproject/win-project-2/Forms/UCComponents/FormChat.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using win_project_2.DAO;
 using win_project_2.Service;
 
+
+
 namespace win_project_2.Forms.UCComponents
 {
     public partial class FormChat : Form
@@ -25,7 +27,32 @@ namespace win_project_2.Forms.UCComponents
             listMessage.Columns.Add("Content", 300);
             listMessage.Columns.Add("Timestamp", 150);
 
+
+
             LoadMessagesToListView(UserDangNhap.userId, _id_receiver);
+            int user1ID = int.Parse(UserDangNhap.userId.ToString());
+            int user2ID = int.Parse(_id_receiver.ToString());
+
+            MessageDAO messageDAO = new MessageDAO();
+            List<Message> messages = messageDAO.LoadMessages(user1ID, user2ID);
+
+            DisplayMessagesInListView(messages);
+        }
+
+        public void DisplayMessagesInListView(List<Message> messages)
+        {
+            listMessage.Items.Clear();
+
+            foreach (var message in messages)
+            {
+                ListViewItem item = new ListViewItem(message.SenderID.ToString());
+
+                item.SubItems.Add(message.Content);
+
+                item.SubItems.Add(message.Timestamp.ToString("g"));
+
+                listMessage.Items.Add(item);
+            }
         }
 
         private void btn_send_Click(object sender, EventArgs e)
@@ -33,6 +60,16 @@ namespace win_project_2.Forms.UCComponents
             MessageDAO messageDAO = new MessageDAO();
             messageDAO.SendMessage(UserDangNhap.userId, _id_receiver, tb_mess.Text);
             tb_mess.Clear();
+
+            int user1ID = int.Parse(UserDangNhap.userId.ToString());
+            int user2ID = int.Parse(_id_receiver.ToString());
+
+            
+            List<Message> messages = messageDAO.LoadMessages(user1ID, user2ID);
+
+
+            // Hiển thị tin nhắn lên ListView
+            DisplayMessagesInListView(messages);
         }
 
         public void LoadMessagesToListView(int senderId, int receiverId)
